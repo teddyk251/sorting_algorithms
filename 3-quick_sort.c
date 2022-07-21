5de729876bf659119df0251c86b658d74abed390
Teddy <teddylnk1@gmail.com>
2022-07-21 14:34:11 +0300

    fix 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,76 +1,80 @@
-
-
 #include "sort.h"
-#include <stdio.h>
 /**
- * quick_sort - sorts an array of integers in ascending order
- * using the quick sort sort algorithm
- * @array: pointer to the array
- * @size: size of the array
-*/
-void quick_sort(int *array, size_t size)
+ * swaps - swaps and prints ints from an array without temp
+ * @array: the array for printing func
+ * @size: the size of array for printing func
+ * @a: one int
+ * @b: the other int
+ */
+void swaps(int *array, size_t size, int *a, int *b)
 {
-recursive_quick_sort(array, size, 0, size - 1);
-}
-
-/**
- * recursive_quick_sort - recursive part
- * @array: array to use
- * @size: size
- * @start: start index
- * @end: end index
-*/
-void recursive_quick_sort(int *array, size_t size, int start, int end)
-{
-int p;
-if (start < end)
-{
-	p = partition(array, size, start, end);
-
-	recursive_quick_sort(array, size, start, p - 1);
-	recursive_quick_sort(array, size, p + 1, end);
-}
-
-}
-
-/**
- * partition - partition the array
- * @array: array to use
- * @size: size
- * @start: start index
- * @end: end index
- * Return: partition index
-*/
-size_t partition(int *array, size_t size, int start, int end)
-{
-int pivot = array[end];
-int i = start - 1;
-int j;
-
-for (j = start; j <= end - 1; j++)
-{
-	if (array[j] < pivot)
+	if (*a != *b)
 	{
-		i++;
-		swap_int1(array, i, j);
-		print_array(array, size);
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
 	}
 }
-swap_int1(array, i + 1, end);
-print_array(array, size);
-return (i+1);
+/**
+ * kwiksort - quick sort via lamuto partition
+ * @array: the array
+ * @size: size of array
+ * @low: low [] of sort
+ * @high: high [] of sort
+ *
+ */
+void kwiksort(int *array, size_t size, ssize_t low, ssize_t high)
+{
+	if (low < high)
+	{
+		size_t part = partition(array, size, low, high);
+
+		kwiksort(array, size, low, part - 1);
+		kwiksort(array, size, part + 1, high);
+	}
 }
 
 /**
- * swap_int1 - swap variable values
- * @array: array to use
- * @a: index 1
- * @b: index 2
-*/
-void swap_int1(int *array, int a, int b)
+ * quick_sort - to quick sort, basically a junk func prototype i had to
+ * embetter
+ * @array: the array to sort
+ * @size: size of array
+ *
+ */
+void quick_sort(int *array, size_t size)
 {
-int tmp;
-tmp = array[a];
-array[a] = array[b];
-array[b] = tmp;
+	/* if bad things, return */
+	if (!array || !size)
+		return;
+	/* else, sort it baby */
+	kwiksort(array, size, 0, size - 1);
+}
+/**
+ * partition - "partition" array
+ * @array: the array
+ * @size: the size of array
+ * @low: the bottom of the index
+ * @high: the top of it
+ * Return: size_t part, the partition value
+ */
+size_t partition(int *array, size_t size, ssize_t low, ssize_t high)
+{
+	/*declarations */
+	int i, j, pivot;
+
+	/* set the pivot */
+	pivot = array[high];
+
+	for (j = low, i = j; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			swaps(array, size, &array[j], &array[i++]);
+		}
+	}
+	/* final swap */
+	swaps(array, size, &array[i], &array[high]);
+	/* the size_t return value here is needed for the part */
+	return (i);
 }
--- sort.h
+++ sort.h
@@ -1,8 +1,10 @@
-#ifndef SORTING_H
-#define SORTING_H
+#ifndef SORT_H
+#define SORT_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/* linked list struct they gave us */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,6 +12,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
 	const int n;
@@ -17,17 +20,27 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* prototypes - theirs*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap_int(int *array, size_t a, size_t b);
-void swap_int1(int *array, int a, int b);
-void recursive_quick_sort(int *array, size_t size, int start, int end);
-size_t partition(int *array, size_t size, int start, int end);
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif
+/* prototypes - mine */
+void swap(int *array, int a, int b);
+void swapper(listint_t *a, listint_t *b);
+size_t knuth_gap(size_t size);
+void kwiksort(int *array, size_t size, ssize_t low, ssize_t high);
+size_t partition(int *array, size_t size, ssize_t low, ssize_t high);
+void swaps(int *array, size_t size, int *a, int *b);
+#endif /* SORT_H */
